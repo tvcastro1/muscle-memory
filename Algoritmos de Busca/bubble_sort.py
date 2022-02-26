7461ff3112605cae1d95de8cbf5e254e9e0f7c4c
tvcastro1 <thiagovieirac@gmail.com>
2022-02-25 21:32:18 -0300

    Inicio da Construção

--- Algoritmos de Busca/bubble_sort.py
+++ Algoritmos de Busca/bubble_sort.py
@@ -1,6 +1,16 @@
 alist = [54,26,93,17,77,31,44,55,20]
 
-for i in range(len(alist)):
-    for x in alist:
-        print(x)
-    print(f"Loop{i + 1}")
+x = 0
+temp = 0
+trocou = False
+
+while x < len(alist) - 1:
+    if alist[x] > alist[x + 1]:
+        temp = alist[x]
+        alist[x] = alist[x +1]
+        alist[x + 1] = temp
+        trocou = True
+    x += 1
+    print(alist)
+
+
