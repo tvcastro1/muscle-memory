916c8656fab2b3be3afb68372299020a2c5256da
tvcastro1 <thiagovieirac@gmail.com>
2022-03-02 16:21:03 -0300

    Bubble Sort "rápido"

--- Algoritmos de Busca/bubble_sort.py
+++ Algoritmos de Busca/bubble_sort.py
@@ -1,14 +1,16 @@
-def bubbleSort(alist):
+def bubbleSort(alist:list) -> list:
     troca = True  # 1 Operador Booleano para fazer o controle da lista já ordenada
-    for passnum in range(len(alist)-1,0,-1):
+    for passnum in range(len(alist) - 1, 0, -1):
         if not troca:
             break
         for i in range(passnum):
             troca = False
-            if alist[i]>alist[i+1]:
-                alist[i], alist[i+1] = alist[i+1], alist[i]
+            if alist[i] > alist[i + 1]:
+                alist[i], alist[i + 1] = alist[i + 1], alist[i]
                 troca = True
 
-alist = [1,2,3,4,5,6]
-bubbleSort(alist)
-print(alist)
+
+if __name__ == "__main__":
+    alist = [1, 2, 3, 4, 5, 6]
+    bubbleSort(alist)
+    print(alist)
