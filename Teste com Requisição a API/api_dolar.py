abc68bb70b46492e440ec4a91947f31a79c23165
tvcastro1 <thiagovieirac@gmail.com>
2022-03-06 13:44:45 -0300

    Consumo de API cotação em dólar

--- Teste com Requisição a API/api_dolar.py
+++ Teste com Requisição a API/api_dolar.py
@@ -1,22 +1,14 @@
 import requests
 
-r = requests.get("http://economia.awesomeapi.com.br/all/USD-BRL")
-cotacao = r.json()
 
-print(cotacao)
-
-print('####Cotação do Dolar####')
-print(cotacao['USD']['low'])
-
-# def main():
-#     r = requests.get("http://economia.awesomeapi.com.br/all/USD-BRL")
-#     if r.status_code == 200:
-#         valor_dolar = r.json()['USD']['low']
-#         print(f'O valor do dólar hoje é R$ {valor_dolar}')
-#     else:
-#         print(f'Ocorreu um erro ao acessar a API {r.status_code}')
-#
-#     if __name__ == '__main__':
-#         main()
+def main():
+    r = requests.get("http://economia.awesomeapi.com.br/all/USD-BRL")
+    if r.status_code == 200:
+        valor_dolar = r.json()['USD']['low']
+        return f'O valor do dólar hoje é R$ {valor_dolar}'
+    else:
+        return f'Ocorreu um erro ao acessar a API {r.status_code}'
 
 
+if __name__ == '__main__':
+    print(main())
