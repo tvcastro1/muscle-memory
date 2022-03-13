d9a8cc2a9f375cf80d7e5b6a23c1199c31f37050
tvcastro1 <thiagovieirac@gmail.com>
2022-03-13 16:13:07 -0300

    Teste Resposta da Api

--- Teste com Requisição a API/api_mercado_pago.py
+++ Teste com Requisição a API/api_mercado_pago.py
@@ -14,37 +14,38 @@ def consulta_config():
     return r.json()
 
 print(consulta_config())
-# def gera_relatorio():
-#
-#     headers = {
-#         'accept': 'application/json',
-#         'content-type': 'application/json',
-#         'Authorization': f'Bearer {token}'
-#     }
-#
-#     data = '{ "begin_date": "2022-02-01T00:00:00Z", "end_date": "2022-02-28T00:00:00Z" }'
-#
-#     r = requests.post('https://api.mercadopago.com/v1/account/bank_report', headers=headers, data=data)
-#     return r
-#
-#
-# print(gera_relatorio())
+def gera_relatorio():
+
+    headers = {
+        'accept': 'application/json',
+        'content-type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+
+    data = '{ "begin_date": "2022-02-01T00:00:00Z", "end_date": "2022-02-28T00:00:00Z" }'
+
+    r = requests.post('https://api.mercadopago.com/v1/account/settlement_report', headers=headers, data=data)
+    return r
 
 
-# headers = {
-#     'accept': 'application/json',
-#     'Authorization': f'Bearer {token} '
-# }
-#
-# response = requests.get('https://api.mercadopago.com/v1/account/bank_report/list', headers=headers)
-#
-# print(response)
+print(gera_relatorio())
 
 
-# headers = {
-#     'Authorization': f'Bearer {token}'
-# }
-#
-# response = requests.get('https://api.mercadopago.com/v1/account/bank_report/:file_name', headers=headers)
-#
-# print(response)
+headers = {
+    'accept': 'application/json',
+    'Authorization': f'Bearer {token} '
+}
+
+response = requests.get('https://api.mercadopago.com/v1/account/settlement_report/list', headers=headers)
+response_json = response.json()
+
+print(response_json)
+
+
+headers = {
+    'Authorization': f'Bearer {token}'
+}
+
+response = requests.get('https://api.mercadopago.com/v1/account/settlement_report/:file_name', headers=headers)
+
+print(response)
